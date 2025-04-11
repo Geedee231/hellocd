a15f5701b9fc3c636a7201eb1d5720428aa066d0
username <user@gmail.com>
2025-04-11 21:25:45 +0000

    adding files created with spring boot generator

---
+++ src/test/java/com/example/helloclad/HellocladApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloclad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellocladApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
